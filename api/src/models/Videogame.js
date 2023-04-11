c84d376f04fe47c0f5b516c625aac7d005a62964
unknown <domirandar@unal.edu.co>
2023-04-11 13:14:55 -0500

    Se modifico el test en la linea 18 donde decia Recipe, en lugar de Videogame. Tambien se crearon los modelos aun sin asociar.

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,35 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: sequelize.UUIDV4,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    platforms:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    image:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    release:{
+      type: DataTypes.DATEONLY,
+      allowNull: false
+    },
+    rating:{
+      type: DataTypes.FLOAT,
+      allowNull: false
+    }
   });
 };
